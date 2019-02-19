8192644f8594ce2db2816125bcf15fcab7a1e348
Michael Mottola <mottola64@gmail.com>
2019-02-19 01:40:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ def countdown
   puts "#{number} SECOND(S)!"
   number -= 1
 end
-"HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
@@ -12,5 +12,5 @@ def countdown_with_sleep
   number -= 1
   sleep(1)
 end
-"HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
 end
