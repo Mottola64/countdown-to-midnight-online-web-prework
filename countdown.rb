5fd369f1e4c87166b81190f1eaf391c497fbe6c9
Michael Mottola <mottola64@gmail.com>
2019-02-19 01:23:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
-
+number = 10
 def countdown
+  while number > 0
+number -= 1
+  puts "#{number} SECOND(S)!"
+end
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  sleep(1)
+end
+"HAPPY NEW YEAR!"
 end
